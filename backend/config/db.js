89eff8a431ef22c5894c783f71708dea651dfba2
T-nisha05 <tanishapandya429@gmail.com>
2026-04-11 11:36:25 +0530

    Removed secrets

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,11 +1,13 @@
+// import mongoose from "mongoose";
+
 import mongoose from "mongoose";
 
 export const connect_db = async function () {
-  await mongoose
-    .connect(
-  "mongodb+srv://tanishapandya429_db_user:test1234@buildresume.8jrjeca.mongodb.net/BuildResume"
-)
-    .then(() => {
-      console.log("Database connected successfully!");
-    });
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+
+    console.log("Database connected successfully!");
+  } catch (error) {
+    console.log("Database connection failed:", error.message);
+  }
 };
